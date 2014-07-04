b7a2b4bc6b838bae8663a0dd8aa8e13b19569635
danilodanicomendes <ddannillo@gmail.com>
2014-07-04 14:56:36 +0100

    Initial Commit

---
+++ LaptopCC/src/laptopcc/LaptopCC.java
@@ -0,0 +1,12 @@
+package laptopcc;
+
+public class LaptopCC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
